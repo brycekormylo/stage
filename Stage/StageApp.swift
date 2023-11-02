6534f5143f68ee694e79feb97791da4fbc01fa3c
Bryce Kormylo <ATLSdev0@gmail.com>
2023-11-02 11:56:04 -0700

    Initial Commit

---
+++ Stage/StageApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageApp.swift
+//  Stage
+//
+//  Created by Bryce on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct StageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
