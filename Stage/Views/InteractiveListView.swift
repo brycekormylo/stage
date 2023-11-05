c4b42dfdf6434b23a9c8b4adcc6abbb997534400
Bryce Kormylo <ATLSdev0@gmail.com>
2023-11-05 11:42:46 -0800

    Big progress on highlights

---
+++ Stage/Views/InteractiveListView.swift
@@ -0,0 +1,18 @@
+//
+//  InteractiveListView.swift
+//  Stage
+//
+//  Created by Bryce on 11/4/23.
+//
+
+import SwiftUI
+
+struct InteractiveListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InteractiveListView()
+}
